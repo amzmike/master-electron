49ef036fb5870794227141ad90fa1f58a9598226
amzmike <bzineddin@outlook.com>
2024-09-04 14:07:15 +0100

    add secondary window

--- main.js
+++ main.js
@@ -1,61 +1,89 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-app.disableHardwareAcceleration();
+const allowDev = false;
+
+// with hardware acceleration disabled, the screen flickers when the window is moved
+// app.disableHardwareAcceleration();
 
 let mainWindow = null;
+let secondaryWindow = null;
+
 let devWindow = null;
+
+app.name = 'RKS-GUI'
+app.version = '0.0.1'
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     x: 0,
     y: 0,
-    width: 800,
+    width: 1600,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
 //       enableRemoteModule: true,
-//       nodeIntegration: true,
+      nodeIntegration: true,
 //       contextIsolation: false,
 //       nodeIntegrationInWorker: true,
 //       nodeIntegrationInSubFrames: true
     }
   })
 
+  secondaryWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    parent: mainWindow,
+    modal: true,
+  })
+
+
 //   require("@electron/remote/main").enable(mainWindow.webContents)
 
   mainWindow.setTitle('RKS-GUI')
-  mainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
-
-  // and load the index.html of the app.
-  // mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
+  secondaryWindow.loadFile(path.join(__dirname, 'secondary.html'))
 
   // Open the DevTools.
-  devWindow = new BrowserWindow()
-  mainWindow.webContents.setDevToolsWebContents(devWindow.webContents)
-  mainWindow.webContents.openDevTools({mode: 'detach'})
-  mainWindow.webContents.once('did-finish-load', () => {
-    let windowBounds = mainWindow.getBounds();
-    devWindow.setPosition(windowBounds.x + windowBounds.width, windowBounds.y)
-  });
+  if (allowDev) {
+    devWindow = new BrowserWindow()
+    mainWindow.webContents.setDevToolsWebContents(devWindow.webContents)
+    mainWindow.webContents.openDevTools({mode: 'detach'})
+    mainWindow.webContents.once('did-finish-load', () => {
+      let windowBounds = mainWindow.getBounds();
+      devWindow.setPosition(windowBounds.x + windowBounds.width, windowBounds.y)
+    });
+    // Set the devtools position when the paretn window is moved
+    mainWindow.on('move', () => {
+      let windowBounds = mainWindow.getBounds()
+      devWindow.setPosition(windowBounds.x + windowBounds.width, windowBounds.y)
+    })
+  }
 
-  // Set the devtools position when the paretn window is moved
-  mainWindow.on('move', () => {
-    let windowBounds = mainWindow.getBounds()
-    devWindow.setPosition(windowBounds.x + windowBounds.width, windowBounds.y)
-  })
-
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   })
+
+  secondaryWindow.on('closed', () => {
+    secondaryWindow = null;
+  })
+
+  if (devWindow) {
+    devWindow.on('closed', () => {
+      devWindow = null;
+    })
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  console.log('6X-GUI is ready')
+  console.log('App name:', app.name)
+  console.log('App version:', app.version)
+
   createWindow()
 
   app.on('activate', function () {
@@ -63,16 +91,28 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+  mainWindow.once('ready-to-show', () => {
+    // mainWindow.show()
+  })
+
+  console.log(app.getPath('userData'));
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
+app.on('before-quit', _e => {
+  // console.log('6X-GUI is quitting')
+})
+
+app.on('browser-window-blur', _e => {
+  // console.log('6X-GUI is blurred')
+})
+
+app.on('browser-window-focus', _e => {
+  // console.log('6X-GUI is focused')
+})
+
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
